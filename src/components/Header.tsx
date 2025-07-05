9748d3832de339b89447421200f46eb26ff75851
김우찬 <dnckss1121@gmail.com>
2025-07-05 22:13:16 +0900

    feat: 헤더바

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import logo from '../assets/logo.svg'
+import Countdown from 'react-countdown';
+
+export default function Header() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-12 flex justify-center border-b border-[#33333A] bg-[#171718]">
+        <div className="flex items-center">
+            <img src={logo} alt="logo" className="h-8" />
+        </div>
+        <div className="flex items-center">
+            <Countdown
+                date={Date.now() + 5 * 60 * 1000}
+                renderer={({ minutes, seconds }) => (
+                <span className="ml-4 h-8 flex items-center text-white text-[28px]">
+                    {minutes < 10 ? `0${minutes}` : minutes}:
+                    {seconds < 10 ? `0${seconds}` : seconds}
+                </span>
+                )}
+                
+            />
+        </div>
+    </div>
+  )
+}
